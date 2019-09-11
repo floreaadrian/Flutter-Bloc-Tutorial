71166892af072e04d64c68e6ed1ae1f10dd7f1ed
floreaadrian <florea.adrian.paul@gmail.com>
2019-09-11 16:55:43 +0300

    done with scroll

---
+++ infinite_scroll/lib/bloc/post_state.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+import 'package:infinite_scroll/models/models.dart';
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class PostState extends Equatable {
+  PostState([List props = const <dynamic>[]]) : super(props);
+}
+
+class PostUninitialized extends PostState {
+  @override
+  String toString() => 'PostUninitialized';
+}
+
+class PostError extends PostState {
+  @override
+  String toString() => 'PostError';
+}
+
+class PostLoaded extends PostState {
+  final List<Post> posts;
+  final bool hasReachedMax;
+
+  PostLoaded({this.posts, this.hasReachedMax}) : super([posts, hasReachedMax]);
+
+  PostLoaded copyWith({
+    List<Post> posts,
+    bool hasReachedMax,
+  }) {
+    return PostLoaded(
+      posts: posts ?? this.posts,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+    );
+  }
+
+  @override
+  String toString() =>
+      'PostLoaded { posts: ${posts.length}, hasReachedMax: $hasReachedMax }';
+}
