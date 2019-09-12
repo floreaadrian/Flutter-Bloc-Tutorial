53a958977ef7731023594134b418ad7dd967104c
floreaadrian <florea.adrian.paul@gmail.com>
2019-09-12 17:21:07 +0300

    done with the login exemple

---
+++ simple_login/lib/login/login_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:simple_login/authentication/bloc.dart';
+import '../user_repository.dart';
+import './bloc.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authenticationBloc,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+          username: event.username,
+          password: event.password,
+        );
+
+        authenticationBloc.dispatch(LoggedIn(token: token));
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
+}
